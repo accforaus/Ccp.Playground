47ba92d2c4517fe031d0fadda50e7aeabd12b32d
sky <1cielo@naver.com>
2019-07-09 22:43:12 +0900

    fix: Use Scanner for solving problem1

--- haneul/#1/p1.kt
+++ haneul/#1/p1.kt
@@ -1,7 +1,16 @@
+import java.util.Scanner
+
 fun main() {
 //    구구단
-    println("Hello, World!")
-    for (x in 2..9)
+    val sc: Scanner = Scanner(System.`in`)
+    println("구구단 출력 프로그램입니다.")
+    println("시작단을 입력하세요: ")
+    var start = sc.nextInt()
+
+    println("마지막단을 입력하세요: ")
+    var end = sc.nextInt()
+
+    for (x in start..end)
         for (y in 1..9)
             println("$x * $y = ${x * y}")
 }
