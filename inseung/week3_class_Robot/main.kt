466d5bb36bdc4df1e942ad6803ea16a4e06ae064
HwangInSeung <chmp1352i$>
2019-07-17 14:13:44 +0900

    Week3 Robot Revised
    Make Package to Robot Class
    
    Change Robot List sorting Comparator to Comparators package and classes
    
    Remove abstract declared members of Robot Class
    
    Add MAP_SIZE constant
    
    Add Input Exception in x, y coordinates and int values for selecting menu options

--- inseung/week3_class_Robot/main.kt
+++ inseung/week3_class_Robot/main.kt
@@ -1,4 +1,8 @@
+import korea.Hwang.ccp.controller.Controller
+
+const val MAP_SIZE = 200
+
 fun main () {
-    val controller = Controller ()
+    val controller = Controller()
     controller.run()
 }
